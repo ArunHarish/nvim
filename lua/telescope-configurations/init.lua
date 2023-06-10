5732c8214dee2a9ab8f4eb221b4a525a20ecdc0c
Arun Harish Balasubramonian <18714272+ArunHarish@users.noreply.github.com>
2023-06-10 18:48:55 +1000

    Telescope list diagnostics only for current buffer. Change leader start key for search

--- lua/telescope-configurations/init.lua
+++ lua/telescope-configurations/init.lua
@@ -4,10 +4,19 @@ local actions = require "telescope.actions"
 
 telescope.setup {
   defaults = {
+    display_stat = { date = true, size = true },
+    layout_config = {
+      horizontal = {
+        prompt_position = 'top',
+        preview_width = 0.70,
+      }
+    },
     path_display = {'smart'},
   },
 	extensions = {
 		file_browser = {
+      git_status = true,
+      hijack_netrw = true,
 			mappings = {
 				i = {
 					["<C-t>"] = actions.select_tab,
@@ -20,15 +29,22 @@ telescope.setup {
 telescope.load_extension('live_grep_args')
 telescope.load_extension('file_browser')
 
-vim.keymap.set('n', '<leader>fr', builtin.resume, {})
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', telescope.extensions.live_grep_args.live_grep_args, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<space>fc', builtin.colorscheme, {})
+vim.keymap.set('n', '<leader>Sr', builtin.resume, {})
+vim.keymap.set('n', '<leader>Sf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>Sg', telescope.extensions.live_grep_args.live_grep_args, {})
+vim.keymap.set('n', '<leader>Sb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>Sc', builtin.colorscheme, {})
+vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
+vim.keymap.set('n', '<leader>dd', function()
+  builtin.diagnostics {
+    bufnr = 0,
+  }
+  end,
+{})
+vim.keymap.set('n', '<leader>dl', builtin.lsp_document_symbols, {})
 vim.keymap.set('n', '<space>fb',  function ()
-		telescope.extensions.file_browser.file_browser {
-			path = "%:p:h"
-		}
+  telescope.extensions.file_browser.file_browser {
+    path = "%:p:h"
+  }
 	end,
 { noremap = true })
-vim.keymap.set('n', '<space>fl', builtin.lsp_document_symbols, {})
