b5a4b0e561abbf2b3648724c4ad5b9f5e688b764
Arun Harish Balasubramonian <18714272+ArunHarish@users.noreply.github.com>
2023-07-03 08:18:57 +1000

    Add buffer number and id

--- lua/tabline-configurations/init.lua
+++ lua/tabline-configurations/init.lua
@@ -2,7 +2,7 @@ vim.opt.termguicolors = true
 require'bufferline'.setup{
   options = {
     mode = 'tabs',
-    tab_size = 25,
+    tab_size = 20,
     diagnostics = 'nvim_lsp',
     max_name_length = 25,
     truncate_names = false,
@@ -11,7 +11,7 @@ require'bufferline'.setup{
       local windowID = vim.api.nvim_tabpage_get_win(opts.id)
       local bufferNumber = vim.fn.winbufnr(windowID)
       if bufferNumber then
-        return opts.raise(bufferNumber)
+        return opts.lower(bufferNumber) .. '.' .. opts.raise(opts.id)
       end
       -- If cannot find any just return empty
       return ''
