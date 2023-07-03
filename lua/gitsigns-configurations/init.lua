e2cf65764ef3531d9bf405b6083151cd6dfe687d
Arun Harish Balasubramonian <18714272+ArunHarish@users.noreply.github.com>
2023-07-03 11:35:32 +1000

    Add keymap for hunk navigation

--- lua/gitsigns-configurations/init.lua
+++ lua/gitsigns-configurations/init.lua
@@ -7,5 +7,28 @@ require'gitsigns'.setup {
     ignore_whitespace = false,
   },
   current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+  on_attach = function (bufnr)
+    local gs = package.loaded.gitsigns
 
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    map('n', ']c', function ()
+      if vim.wo.diff then return ']c' end
+      vim.schedule(function ()
+        gs.next_hunk()
+      end)
+    end)
+
+    map('n', '[c', function ()
+      if vim.wo.diff then return ']c' end
+      vim.schedule(function ()
+        gs.prev_hunk()
+      end)
+    end)
+
+  end
 }
