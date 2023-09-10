796997973745f7bafc820b19fb25390661e30470
Arun Harish Balasubramonian <18714272+ArunHarish@users.noreply.github.com>
2023-09-11 09:30:07 +1000

    Add hit condition, log point and breakpoint condition in vimspector

--- lua/vimspector-configurations/init.lua
+++ lua/vimspector-configurations/init.lua
@@ -6,7 +6,49 @@ vim.keymap.set('n', '<leader>Dn', function() vim.call('vimspector#StepOver') end
 vim.keymap.set('n', '<leader>Dr', function() vim.call('vimspector#Restart') end, {})
 vim.keymap.set('n', '<leader>Dx', function() vim.call('vimspector#Reset') end, {})
 vim.keymap.set('n', '<leader>Dbt', function() vim.call('vimspector#ToggleBreakpoint') end, {})
+vim.keymap.set('n', '<leader>DbH', function()
+  vim.ui.input({
+    prompt = "Enter your hit condition: ",
+    default = "",
+  },
+    function(input)
+      if input then
+        vim.fn["vimspector#ToggleBreakpoint"]({
+          hitCondition = input,
+        })
+      end
+    end
+  )
+end, {})
+vim.keymap.set('n', '<leader>DbL', function()
+  vim.ui.input({
+    prompt = "Enter your logpoint: ",
+    default = "",
+  },
+    function(input)
+      if input then
+        vim.fn["vimspector#ToggleBreakpoint"]({
+          logMessage = input,
+        })
+      end
+    end
+  )
+end, {})
+vim.keymap.set('n', '<leader>DbC', function()
+  vim.ui.input({
+    prompt = "Enter your breakpoint condition: ",
+    default = "",
+  },
+    function(input)
+      if input then
+        vim.fn["vimspector#ToggleBreakpoint"]({
+          condition = input,
+        })
+      end
+    end
+  )
+end, {})
 vim.keymap.set('n', '<leader>Dbc', function() vim.call('vimspector#ClearBreakpoints') end, {})
 vim.keymap.set('n', '<leader>Dbl', function() vim.call('vimspector#ListBreakpoints') end, {})
-vim.keymap.set('n', '<leader>]Db', function() vim.call('vimspector#JumpToNextBreakpoint') end, {})
-vim.keymap.set('n', '<leader>[Db', function() vim.call('vimspector#JumpToPreviousBreakpoint') end, {})
+vim.keymap.set('n', '<leader>Db]', function() vim.call('vimspector#JumpToNextBreakpoint') end, {})
+vim.keymap.set('n', '<leader>Db[', function() vim.call('vimspector#JumpToPreviousBreakpoint') end, {})
