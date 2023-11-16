096a3a9cb0828f1f412124ae7632d70235843427
Arun Harish Balasubramonian <18714272+ArunHarish@users.noreply.github.com>
2023-11-16 10:08:01 +1000

    Add multi-select delete of tab pages

--- lua/telescope-configurations/custom-extensions/init.lua
+++ lua/telescope-configurations/custom-extensions/init.lua
@@ -1,6 +1,7 @@
 local pickers = require('telescope.pickers')
 local finders = require('telescope.finders')
 local utils = require('telescope.utils')
+local action_utils = require('telescope.actions.utils')
 local conf = require('telescope.config').values
 local actions = require('telescope.actions')
 local action_state = require('telescope.actions.state')
@@ -8,58 +9,66 @@ local previewers = require('telescope.previewers')
 
 local CustomExtensions = {}
 
-local WindowTabPagesIterator = function ()
-
+local get_window_tab_pages = function ()
+  local table_list = {}
+  local current_window = vim.api.nvim_get_current_win()
   local tabpages = vim.api.nvim_list_tabpages()
-  local tabpageIndex = 1
-  local windowIndex = 1
 
-  return function ()
-
-    if tabpageIndex > #tabpages then
-      return
-    end
-
-    -- List all windows 
-    local tabpage = tabpages[tabpageIndex]
+  -- for each tab page find its windows
+  for _, tabpage in ipairs(tabpages) do
+    -- List all windows
     local windowList = vim.api.nvim_tabpage_list_wins(tabpage)
-    local windowID = windowList[windowIndex]
+    for _, window in ipairs(windowList) do
+      local buffer = vim.api.nvim_win_get_buf(window)
+      local bufferPath = vim.api.nvim_buf_get_name(buffer)
 
-    -- Current buffer in the window
-    local buffer = vim.api.nvim_win_get_buf(windowID)
-    local bufferPath = vim.api.nvim_buf_get_name(buffer)
-
-    windowIndex = windowIndex + 1
-
-    if windowIndex > #windowList then
-      windowIndex = 1
-      tabpageIndex = tabpageIndex + 1
+      if window ~= current_window then
+        table.insert(table_list, {
+          tabpage = tabpage,
+          window = window,
+          buffer = buffer,
+          path = bufferPath,
+        })
+      end
     end
-
-    return {
-      tabpage = tabpage,
-      window = windowID,
-      buffer = buffer,
-      path = bufferPath,
-    }
   end
+
+  return table_list
 end
 
+local attach_mapping_listeners = function(prompt_number)
+  return {
+    on_select = function()
+      actions.close(prompt_number)
+      local selection = action_state.get_selected_entry()
+      -- Go to selected window
+      vim.api.nvim_set_current_win(selection.windowID)
+    end,
+    on_selection_delete = function()
+      action_utils.map_selections(prompt_number, function(selection)
+        utils.win_delete(nil, selection.windowID, true, false)
+      end)
+      actions.close(prompt_number)
+    end
+  }
+end
+
+
 CustomExtensions.search_tab_pages = function ()
-  local tabpageList = {}
+  local tabpageList = get_window_tab_pages()
   local config = {}
 
-  for window in WindowTabPagesIterator() do
-    table.insert(tabpageList, window)
-  end
-
   pickers.new(config, {
-    prompt_title = 'Tabpages',
+    prompt_title = 'Showing other windows',
     finder = finders.new_table {
       results = tabpageList,
       entry_maker = function(entry)
         local devicons = utils.get_devicons(entry.path)
-        local basename = utils.path_smart(entry.path)
+        local basename = utils.transform_path({
+          path_display = {
+            absolute = false,
+          }
+        }, entry.path)
         local display = string.format("%d %s %s", entry.tabpage, devicons, basename)
         return {
           display = display,
@@ -71,13 +80,13 @@ CustomExtensions.search_tab_pages = function ()
       end
     },
     sorter = conf.file_sorter(config),
-    attach_mappings = function(prompt_number)
-      actions.select_default:replace(function ()
-        actions.close(prompt_number)
-        local selection = action_state.get_selected_entry()
-        -- Go to selected window
-        vim.api.nvim_set_current_win(selection.windowID)
-      end)
+    attach_mappings = function(prompt_number, map)
+      local listeners = attach_mapping_listeners(prompt_number)
+
+      map('i', '<CR>', listeners.on_select)
+      map('i', '<C-d>', listeners.on_selection_delete)
+      map('n', '<C-d>', listeners.on_selection_delete)
+
       return true
     end,
     previewer = previewers.new_buffer_previewer {
