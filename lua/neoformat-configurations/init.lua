7aecb0c8a3280e977211d257f53005feaf778a6c
Arun Harish Balasubramonian <18714272+ArunHarish@users.noreply.github.com>
2024-05-27 11:17:36 +1000

    Add mjs as allowed neoformat extension

--- lua/neoformat-configurations/init.lua
+++ lua/neoformat-configurations/init.lua
@@ -1,6 +1,6 @@
 vim.cmd[[let g:neoformat_try_node_exe=1]]
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  pattern = { "*.ts", "*.js", "*.tsx", "*.jsx" },
+  pattern = { "*.ts", "*.js", "*.tsx", "*.jsx", "*.mjs" },
   callback = function()
     vim.cmd("Neoformat prettier")
     if vim.fn.exists(':EslintFixAll') > 0 then
