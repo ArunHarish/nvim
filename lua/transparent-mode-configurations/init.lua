73eefb89fdd6dd1ccd5bc2fdd99f26065ae86821
Arun Harish Balasubramonian <18714272+ArunHarish@users.noreply.github.com>
2024-05-29 23:20:28 +1000

    Set new bg and fg color for visual selection

--- lua/transparent-mode-configurations/init.lua
+++ lua/transparent-mode-configurations/init.lua
@@ -1,5 +1,4 @@
--- This only works for catpuccin
-vim.api.nvim_set_hl(0, 'Visual', { bg = 'black', fg = '#ffffff' })
+vim.api.nvim_set_hl(0, 'Visual', { bg='#d1e2f8', fg='#0969da', bold=true })
 vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'GitSignsCurrentLineBlame', { bg = 'none', fg = '#999999', italic = true })
 vim.api.nvim_set_hl(0, 'LineNr', { bg = 'none', fg = '#999999' })
