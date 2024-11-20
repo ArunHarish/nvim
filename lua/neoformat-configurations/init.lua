cca1f6fd4957496cc5585dace0141762ce6882f5
Arun Harish Balasubramonian <18714272+ArunHarish@users.noreply.github.com>
2024-11-20 14:29:05 +1000

    Choose formatter based on biome lsp status

--- lua/neoformat-configurations/init.lua
+++ lua/neoformat-configurations/init.lua
@@ -1,8 +1,18 @@
 vim.cmd[[let g:neoformat_try_node_exe=1]]
+local function choose_formatter(event)
+  local clients = vim.lsp.get_clients({ bufnr = event.bufnr })
+  for _, client in pairs(clients) do
+   if client.name == "biome" and client.root_dir and string.len(client.root_dir) > 0 then
+      vim.cmd("Neoformat biome")
+      return
+   end
+  end
+  vim.cmd("Neoformat prettier")
+end
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   pattern = { "*.ts", "*.js", "*.tsx", "*.jsx", "*.mjs" },
-  callback = function()
-    vim.cmd("Neoformat prettier")
+  callback = function(event)
+    choose_formatter(event)
     if vim.fn.exists(':EslintFixAll') > 0 then
       vim.cmd("EslintFixAll")
     else
