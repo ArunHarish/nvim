69d21abbcfc82677ff026d26e950ca1ba244e9ff
ArunHarish <arunhari97@yahoo.com>
2024-11-29 22:20:03 +1000

    Include signal logic to listen for changes on freedesktop appearance settings

--- theme-listener/src/main.rs
+++ theme-listener/src/main.rs
@@ -2,14 +2,55 @@ use std::time::Duration;
 extern crate dbus;
 
 use dbus::blocking::{ Connection, Proxy };
-use dbus::arg;
+use dbus::{ arg, Message };
 
 enum Theme {
     DARK,
     LIGHT,
 }
 
-fn retrieve_freedesktop_theme(conn: Connection) -> Result<Theme, dbus::Error> {
+pub struct OrgFreeDesktopPortalDesktop {
+    pub sender: String,
+    pub key: String,
+}
+
+impl arg::AppendAll for OrgFreeDesktopPortalDesktop {
+    fn append(&self, i: &mut arg::IterAppend) {
+        arg::RefArg::append(&self.sender, i);
+    }
+}
+
+impl arg::ReadAll for OrgFreeDesktopPortalDesktop {
+    fn read(i: &mut arg::Iter) -> Result<Self, arg::TypeMismatchError> {
+        Ok(OrgFreeDesktopPortalDesktop {
+            sender: i.read()?,
+            key: i.read()?,
+            // value: i.read()?,
+        })
+    }
+}
+
+impl dbus::message::SignalArgs for OrgFreeDesktopPortalDesktop {
+    const NAME: &'static str = "SettingChanged";
+    const INTERFACE: &'static str = "org.freedesktop.portal.Settings";
+}
+
+fn signal_listen_freedesktop_theme() -> Result<bool, dbus::Error> {
+    let connection = Connection::new_session()?;
+    let proxy = connection.with_proxy("org.freedesktop.portal.Desktop", "/org/freedesktop/portal/desktop", Duration::from_millis(5000));
+
+    let _ = proxy.match_signal(|h: OrgFreeDesktopPortalDesktop, _: &Connection, _: &Message| {
+        if h.sender == "org.freedesktop.appearance" && h.key == "color-scheme" {
+            println!("Sender {} {}", h.sender, h.key);
+        }
+        true
+    });
+
+    loop { connection.process(Duration::from_millis(1000))?; }
+}
+
+fn retrieve_freedesktop_theme() -> Result<Theme, dbus::Error> {
+    let conn = Connection::new_session()?;
     let proxy = Proxy::new("org.freedesktop.portal.Desktop", "/org/freedesktop/portal/desktop", Duration::from_millis(5000), &conn);
     let result: (arg::Variant<Box<dyn arg::RefArg>>,) = proxy.method_call("org.freedesktop.portal.Settings", "Read", ("org.freedesktop.appearance", "color-scheme",))?;
 
@@ -25,24 +66,27 @@ fn retrieve_freedesktop_theme(conn: Connection) -> Result<Theme, dbus::Error> {
     }
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let theme: Theme = if std::env::consts::OS == "linux" {
-        let conn = Connection::new_session()?;
-        retrieve_freedesktop_theme(conn)?
-    } else if std::env::consts::OS == "macos" {
-        Theme::LIGHT
-    } else {
-        Theme::LIGHT
-    };
-
+fn output_theme(theme: Theme) {
     match theme {
         Theme::DARK => {
             println!("dark");
-        },
+        }
         Theme::LIGHT => {
             println!("light");
         }
     }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    if std::env::consts::OS == "linux" {
+        let theme = retrieve_freedesktop_theme()?;
+        output_theme(theme);
+        signal_listen_freedesktop_theme()?;
+    } else if std::env::consts::OS == "macos" {
+        output_theme(Theme::LIGHT);
+    } else {
+        output_theme(Theme::DARK);
+    };
 
     Ok(())
 }
