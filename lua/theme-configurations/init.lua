692034230679956dc5ed2e959352ad17bcdbb9f7
ArunHarish <arunhari97@yahoo.com>
2024-11-29 22:19:37 +1000

    Remove neovim theme switch logic

--- lua/theme-configurations/init.lua
+++ lua/theme-configurations/init.lua
@@ -16,13 +16,7 @@ vim.fn.jobstart({ table.concat(shellPath, '/') }, {
   stdout_buffered = false,
   on_stdout = function(_, data)
     if data[1] == "dark" then
-      vim.cmd.colorscheme('github_dark')
-      vim.api.nvim_set_hl(0, 'Visual', { bg='#d1e2f8', fg='#0969da', bold=true })
-      vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-      vim.api.nvim_set_hl(0, 'GitSignsCurrentLineBlame', { bg = 'none', fg = '#999999', italic = true })
-      vim.api.nvim_set_hl(0, 'LineNr', { bg = 'none', fg = '#999999' })
     elseif data[1] == "light" then
-      vim.cmd.colorscheme('github_light')
     end
   end
 })
