cad819315e0f4dbec1c4442263c0dc4347aa68bf
Arun Harish Balasubramonian <18714272+ArunHarish@users.noreply.github.com>
2025-01-03 10:30:59 +1000

    Remove neoformat configuration

---
+++ lua/format-configurations/init.lua
@@ -0,0 +1,11 @@
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = { "*.ts", "*.js", "*.tsx", "*.jsx", "*.mjs" },
+  callback = function(event)
+    vim.lsp.buf.format()
+    if vim.fn.exists(':EslintFixAll') > 0 then
+      vim.cmd("EslintFixAll")
+    else
+      print('Warning: Eslint not found')
+    end
+  end
+})
