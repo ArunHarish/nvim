158728a608bf3a71b36216e5ce311aa587e51310
Arun Harish Balasubramonian <18714272+ArunHarish@users.noreply.github.com>
2025-07-29 10:20:13 +1000

    Separate Rust and JS formats. Use prettier if found

--- lua/format-configurations/init.lua
+++ lua/format-configurations/init.lua
@@ -1,11 +1,31 @@
+-- Rust format
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  pattern = { "*.ts", "*.js", "*.tsx", "*.jsx", "*.mjs", "*.rs" },
+  pattern = { "*.rs" },
   callback = function(event)
-    vim.lsp.buf.format()
+    vim.lsp.buf.format({ bufnr = event.bufnr })
+  end
+})
+
+-- Typescript/Javascript formats
+vim.cmd[[let g:neoformat_try_node_exe=1]]
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = { "*.ts", "*.js", "*.tsx", "*.jsx", "*.mjs" },
+  callback = function(event)
     if vim.fn.exists(':EslintFixAll') > 0 then
       vim.cmd("EslintFixAll")
     else
       print('Warning: Eslint not found')
+    end
+
+    local biome_client = vim.lsp.get_clients({
+      bufnr = event.bufnr,
+      name = "biome",
+    })
+
+    if #biome_client > 0 then
+      vim.lsp.buf.format()
+    else
+      vim.cmd("Neoformat prettier")
     end
   end
 })
