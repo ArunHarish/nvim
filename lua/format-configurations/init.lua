03a6fdb2e895297f078a2cb3f65b514ef825f812
Arun Harish Balasubramonian <18714272+ArunHarish@users.noreply.github.com>
2025-11-10 20:24:43 +1000

    Use lsp buf format if eslint client is attached

--- lua/format-configurations/init.lua
+++ lua/format-configurations/init.lua
@@ -11,18 +11,18 @@ vim.cmd[[let g:neoformat_try_node_exe=1]]
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   pattern = { "*.ts", "*.js", "*.tsx", "*.jsx", "*.mjs" },
   callback = function(event)
-    if vim.fn.exists(':EslintFixAll') > 0 then
-      vim.cmd("EslintFixAll")
-    else
-      print('Warning: Eslint not found')
-    end
-
     local biome_client = vim.lsp.get_clients({
       bufnr = event.bufnr,
       name = "biome",
     })
 
-    if #biome_client > 0 then
+    local eslint_client = vim.lsp.get_clients({
+      bufnr = event.bufnr,
+      name = "eslint",
+    })
+
+    -- Use biome if it's available else check eslint otherwise use neoformat
+    if #biome_client > 0 or #eslint_client > 0 then
       vim.lsp.buf.format()
     else
       vim.cmd("Neoformat prettier")
